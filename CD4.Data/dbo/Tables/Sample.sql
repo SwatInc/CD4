7b9c3c54f623af874d3a8998cb05ce3464bc4986
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-04-27 04:50:36 +0500

    Added FK AnalysisRequestId and renamed FK column Site to SiteId

--- CD4.Data/dbo/Tables/Sample.sql
+++ CD4.Data/dbo/Tables/Sample.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[Sample]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Cin] VARCHAR(50) NOT NULL, 
-    [Site] INT NOT NULL DEFAULT 0, -- 0 is UNKNOWN on SITES TABLE
+    [Cin] VARCHAR(50) NOT NULL,
+    [AnalysisRequestId] INT NOT NULL , 
+    [SiteId] INT NOT NULL DEFAULT 0, -- 0 is UNKNOWN on SITES TABLE
     [CollectionDate] DATE NULL , 
     [ReceivedDate] DATE NOT NULL DEFAULT GETDATE(), 
-    CONSTRAINT [AK_Sample_Cin] UNIQUE ([Cin]) 
-)
+
+    CONSTRAINT [AK_Sample_Cin] UNIQUE ([Cin]), 
+    CONSTRAINT [FK_Sample_Sites] FOREIGN KEY ([SiteId]) REFERENCES [dbo].[Sites]([Id]), 
+    CONSTRAINT [FK_Sample_ToTable] FOREIGN KEY ([AnalysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]))
