d733c5363a94c8f39e60ee3a6515aa95137aec64
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-04-27 04:34:45 +0500

    Added an FK column Sample_Cin

--- CD4.Data/dbo/Tables/Result.sql
+++ CD4.Data/dbo/Tables/Result.sql
@@ -1,16 +1,11 @@
 ﻿CREATE TABLE [dbo].[Result]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [AnalysisRequestId] INT NOT NULL, 
+    [AnalysisRequestId] INT NOT NULL,
+    [Sample_Cin] INT NOT NULL,
     [TestId] INT NOT NULL, 
     [Result] VARCHAR(50) NULL, 
-    [ResultDate] DATE NULL --if result not null, get system date GETDATE()
-, 
+    [ResultDate] DATE NULL, --if result not null, get system date GETDATE()   
     CONSTRAINT [FK_Result_AnalysisRequest] FOREIGN KEY ([AnalysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]), 
-    CONSTRAINT [FK_Result_Test] FOREIGN KEY ([TestId]) REFERENCES [dbo].[Test]([Id]))
-/*
-AnalysisRequest.ID
-Test.Id
-Result
-ResultDate
-*/
+    CONSTRAINT [FK_Result_Test] FOREIGN KEY ([TestId]) REFERENCES [dbo].[Test]([Id]), 
+    CONSTRAINT [FK_Result_Sample] FOREIGN KEY ([Sample_Cin]) REFERENCES [dbo].[Sample]([Id]))
