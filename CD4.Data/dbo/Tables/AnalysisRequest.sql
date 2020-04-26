632eb63f602275ed3e343dfd7871bf4e52aa07a0
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-04-27 04:30:47 +0500

    Added an FK column to Clinical details table and removed Cin colmn

--- CD4.Data/dbo/Tables/AnalysisRequest.sql
+++ CD4.Data/dbo/Tables/AnalysisRequest.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [dbo].[AnalysisRequest]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Cin] VARCHAR(50) NOT NULL,
 	PatientId INT NOT NULL, 
     [Age] INT NULL, 
     [CheckedBy] INT NOT NULL DEFAULT 0, -- Zero Will be NA on scientist table
     [ApprovedBy] INT NOT NULL DEFAULT 0 -- Zero will be NA on scientist table
-, 
+,   [ClinicalDetailId] INT NOT NULL DEFAULT 0, -- Zero Will be NA on scientist table
+
     CONSTRAINT [FK_AnalysisRequest_Patient] FOREIGN KEY ([PatientId]) REFERENCES [dbo].[Patient]([Id]), 
     CONSTRAINT [FK_AnalysisRequest_ScientistChecked] FOREIGN KEY ([CheckedBy]) REFERENCES [dbo].[Scientist]([Id]),
-    CONSTRAINT [FK_AnalysisRequest_ScientistApproved] FOREIGN KEY ([ApprovedBy]) REFERENCES [dbo].[Scientist]([Id]))
+    CONSTRAINT [FK_AnalysisRequest_ScientistApproved] FOREIGN KEY ([ApprovedBy]) REFERENCES [dbo].[Scientist]([Id]),
+    CONSTRAINT [FK_AnalysisRequest_ClinicalDetail] FOREIGN KEY ([ClinicalDetailId]) REFERENCES [dbo].[ClinicalDetail]([Id]))
