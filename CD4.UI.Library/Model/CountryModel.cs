155dc38255310e7c58505bad98d8a8a1f903cee3
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-04-28 00:27:34 +0500

    Added and completed Country model. Added and partially completed CountryViewModel

---
+++ CD4.UI.Library/Model/CountryModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CD4.UI.Library.Model
+{
+    public class CountryModel : INotifyPropertyChanged
+    {
+        private int id;
+        private string country;
+
+        public int Id
+        {
+            get => id; set
+            {
+                if (id == value) return;
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Country
+        {
+            get => country; set
+            {
+                if (country == value) return;
+                country = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #region INotifyPropertyChanged Hookup
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
---
+++ CD4.UI.Library/ViewModel/CountryViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CD4.UI.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.UI.Library.ViewModel
+{
+    public class CountryViewModel: INotifyPropertyChanged
+    {
+        public event EventHandler<String> PushingLogs;
+        public event EventHandler<string> PushingMessages;
+
+        #region INotifyPropertyChanged Hookup
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public BindingList<CountryModel> CountryList { get; set; }
+        public CountryModel SelectedRow { get; set; }
+    }
+}
