a4916ed3ac8f60f374b834d693dbeb34f030a069
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-04-27 05:13:59 +0500

    Added FK col CountryId and FK constraint. Changed the size of the col [FullName] to VARCHAR(100) from  VARCHAR(200

--- CD4.Data/dbo/Tables/Patient.sql
+++ CD4.Data/dbo/Tables/Patient.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [dbo].[Patient]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [FullName] VARCHAR(200) NULL, 
+    [FullName] VARCHAR(100) NULL, 
     [NidPp] VARCHAR(50) NULL,
     [Age] INT NULL, 
     [Birthdate] DATE NULL,  
     [GenderId] INT NOT NULL, --MALE, FEMALE, UNKNOWN
     [AtollId] INT NOT NULL DEFAULT 0,  -- 0 will be as UNKNOWN Atoll table
+    [CountryId] INT NOT NULL DEFAULT 0, -- 0 will be as UNKNOWN Country table
     [Address] VARCHAR(100) NULL, 
     [PhoneNumber] VARCHAR(20) NULL, 
     CONSTRAINT [FK_Patient_Gender] FOREIGN KEY ([GenderId]) REFERENCES [dbo].[Gender]([Id]), 
     CONSTRAINT [AK_Patient_NidPp] UNIQUE ([NidPp]), 
-    CONSTRAINT [FK_Patient_Atoll] FOREIGN KEY ([AtollId]) REFERENCES [dbo].[Atoll]([Id]))
+    CONSTRAINT [FK_Patient_Atoll] FOREIGN KEY ([AtollId]) REFERENCES [dbo].[Atoll]([Id]), 
+    CONSTRAINT [FK_Patient_Country] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[Country]([Id]))
