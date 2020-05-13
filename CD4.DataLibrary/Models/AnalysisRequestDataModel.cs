bebb9586f1d95a71a58432124c66a4903e3c1482
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-14 01:08:26 +0500

    Added analysis request data model to data library

---
+++ CD4.DataLibrary/Models/AnalysisRequestDataModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.Models
+{
+    public class AnalysisRequestDataModel
+    {
+        #region Request Fields
+        public string Cin { get; set; }
+        public int SelectedSiteId { get; set; }
+        public DateTime SampleCollectionDate { get; set; }
+        public DateTime SampleReceivedDate { get; set; }
+
+        #endregion
+
+        #region Patient details
+        public string NationalIdPassport { get; set; }
+        public string Fullname { get; set; }
+        public int GenderId { get; set; }
+        public string Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Address { get; set; }
+        public int AtollId { get; set; } //IslandId is not required
+        public int CountryId { get; set; }
+
+        #endregion
+
+        #region Clinical Details, Tests, Episode Number
+        public List<ClinicalDetailsOrderEntryModel> ClinicalDetails { get; set; }
+        public List<TestModel> Tests { get; set; }
+        public string EpisodeNumber { get; set; }
+
+        #endregion
+    }
+}
