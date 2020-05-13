bbf23cbc6d0102460092463925df2e7bfb9e6964
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-13 09:34:45 +0500

    added stored procedure for fetching patient data with national Id  / passport number

---
+++ CD4.Data/dbo/StoredProcedures/Copy of usp_SearchPatientByPartialName.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[usp_SearchPatientByNidPp]
+	@NidPp varchar(50)
+AS
+BEGIN
+SET NOCOUNT ON;
+SELECT [P].[Id]
+, [P].[FullName]
+, [P].[NidPp]
+, [P].[Birthdate]
+, [G].[Gender]
+, [A].[Atoll]
+, [A].[Island]
+, [C].[Country]
+, [P].[Address]
+, [P].[PhoneNumber]
+FROM [dbo].[Patient] [P]
+INNER JOIN [dbo].[Atoll] [A] ON [P].[AtollId] = [A].[Id]
+INNER JOIN [dbo].[Gender] [G] ON [P].[GenderId] = [G].[Id]
+INNER JOIN [dbo].[Country] [C] ON [P].[CountryId] = [C].[Id]
+WHERE [P].[NidPp] = @NidPp;
+END
