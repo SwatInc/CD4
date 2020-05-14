56bdcb8f0900dd64b785777631e6550c07b1ee50
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-15 02:14:47 +0500

    Added stored procedure to fetch clinical details by request Id

---
+++ CD4.Data/dbo/StoredProcedures/usp_GetClinicalDetailsByRequestId.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetClinicalDetailsByRequestId]
+	@AnalysisRequestId int
+AS
+BEGIN
+SET NOCOUNT ON;
+SELECT * 
+FROM [dbo].[AnalysisRequest_ClinicalDetail] [C]
+WHERE [C].[AnalysisRequestId] = @AnalysisRequestId;
+END
