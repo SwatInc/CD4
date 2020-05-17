1cc2c8c44f5340f9b7df735d5abaaf37299b756f
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-17 22:17:19 +0500

    Added the stored procedure to data to dbo.AnalysisRequest

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertAnalysisRequest.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertAnalysisRequest]
+	@PatientId int,
+	@EpisodeNumber varchar(15),
+	@Age varchar(20)
+AS
+BEGIN
+SET NOCOUNT ON;
+	INSERT INTO [dbo].[AnalysisRequest]([PatientId], [EpisodeNumber], [Age])
+	OUTPUT INSERTED.Id
+	VALUES (@PatientId, @EpisodeNumber, @Age);
+END
