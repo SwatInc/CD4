33963e9083bc870000738b02c6d1a3c6bc8f05d7
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-17 11:03:28 +0500

    Added database models for patient insert and update

---
+++ CD4.DataLibrary/Models/PatientInsertDatabaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.Models
+{
+	public class PatientInsertDatabaseModel
+	{
+		public string Fullname { get; set; }
+		public string NidPp { get; set; }
+		public string Birthdate { get; set; }
+		public int GenderId { get; set; }
+		public int AtollId { get; set; }
+		public int CountryId { get; set; }
+		public string Address { get; set; }
+		public string PhoneNumber { get; set; }
+	}
+}
---
+++ CD4.DataLibrary/Models/PatientUpdateDatabaseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CD4.DataLibrary.Models
+{
+    public class PatientUpdateDatabaseModel
+	{
+		public int Id { get; set; }
+		public string Fullname { get; set; }
+		public string NidPp { get; set; }
+		public string Birthdate { get; set; }
+		public int GenderId { get; set; }
+		public int AtollId { get; set; }
+		public int CountryId { get; set; }
+		public string Address { get; set; }
+		public string PhoneNumber { get; set; }
+	}
+}
