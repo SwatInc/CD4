4592548706d9526c1898bbf0b86450372f51aa1f
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-18 04:14:50 +0500

    Added a stored procedure to add ARs, Samples and requested tests to the database at the same time

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertAnalysisRequestSampleAndRequestedTests.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertAnalysisRequestSampleAndRequestedTests]
+	@PatientId int,
+	@EpisodeNumber varchar(15),
+	@Age varchar(20),
+	@Cin varchar(50),
+	@AnalysisRequestId int,
+	@SiteId int,
+	@CollectionDate char(8),
+	@ReceivedDate char(8),
+	@requestedTestData [dbo].[ResultTableInsertDataUDT] READONLY
+AS
+BEGIN
+SET NOCOUNT ON;
+
+	--used to catch all inserted ids
+	DECLARE @InsertedId TABLE ([Id] int);
+
+	--insert into dbo.AnalysisRequest
+	INSERT INTO [dbo].[AnalysisRequest]([PatientId], [EpisodeNumber], [Age])
+	OUTPUT INSERTED.Id INTO @InsertedId
+	VALUES (@PatientId, @EpisodeNumber, @Age);
+
+	--set inserted analysis requestId
+	SELECT @AnalysisRequestId =  [Id] FROM @InsertedId;
+
+	--insert into dbo.Sample
+	INSERT INTO [dbo].[Sample]([Cin], [AnalysisRequestId], [SiteId], [CollectionDate], [ReceivedDate])
+	VALUES (@Cin,@AnalysisRequestId , @SiteId, @CollectionDate, @ReceivedDate);
+
+	--insert into dbo.Result
+	INSERT INTO [dbo].[Result] ([Sample_Cin], [TestId])
+	SELECT [TD].[Sample_Cin], [TD].[TestId]
+	FROM @requestedTestData [TD];
+
+END
--- CD4.Data/dbo/Types/ResultTableInsertDataUDT.sql
+++ CD4.Data/dbo/Types/ResultTableInsertDataUDT.sql
@@ -2,5 +2,5 @@
 CREATE TYPE [dbo].[ResultTableInsertDataUDT] AS TABLE
 (
 	[TestId] INT NOT NULL PRIMARY KEY, 
-	[Sample_Cin] VARCHAR(50)
+	[Sample_Cin] VARCHAR(50) NOT NULL
 )
