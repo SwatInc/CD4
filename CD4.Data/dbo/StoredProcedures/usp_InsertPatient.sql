84c9c5f99b6d48f6560eb0ea00a0dfe3781d23e5
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-17 10:33:49 +0500

    Added the stored procedure to insert a patient

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertPatient.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertPatient]
+	@Fullname varchar(100),
+	@NidPp varchar(50),
+	@Birthdate char(8),
+	@GenderId int,
+	@AtollId int,
+	@CountryId int,
+	@Address varchar(100),
+	@PhoneNumber varchar(20)
+AS
+BEGIN
+SET NOCOUNT ON;
+	INSERT INTO [dbo].[Patient] ([FullName], [NidPp], [Birthdate],[GenderId], [AtollId], [CountryId], [Address], [PhoneNumber])
+	OUTPUT INSERTED.Id
+	VALUES (@Fullname, @NidPp, @Birthdate, @GenderId, @AtollId, @CountryId, @Address);
+END
