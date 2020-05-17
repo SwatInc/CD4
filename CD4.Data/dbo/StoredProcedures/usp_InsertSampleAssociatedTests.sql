6d2ce8b90d8ef7559499ecf6671e6c301e71fedf
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-18 00:47:13 +0500

    Added stored procedure to data row to dbo.Samples

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertSampleAssociatedTests.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertSampleAssociatedTests]
+	@AnalysisRequestId int,
+	@Sample_Cin varchar(50),
+	@TestId int
+	--Result and result date is null
+AS
+BEGIN
+SET NOCOUNT ON;
+	INSERT INTO [dbo].[Result] ([AnalysisRequestId], [Sample_Cin], [TestId])
+	OUTPUT INSERTED.Id
+	VALUES (@AnalysisRequestId, @Sample_Cin ,@TestId);
+END
