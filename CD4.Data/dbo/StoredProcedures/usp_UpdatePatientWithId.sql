94b3550a51aadb4ff907f01237d9f32cb048b3c8
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-17 10:42:31 +0500

    stored procedure for updating patient

---
+++ CD4.Data/dbo/StoredProcedures/usp_UpdatePatientWithId.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[usp_UpdatePatientWithId]
+	@Id int,
+	@Fullname varchar(100),
+	@NidPp varchar(50),
+	@Birthdate char(8),
+	@GenderId int,
+	@AtollId int,
+	@CountryId int,
+	@Address varchar(100),
+	@PhoneNumber varchar(20)
+AS
+BEGIN
+SET NOCOUNT ON;
+	UPDATE [dbo].[Patient]
+	SET [FullName] = @Fullname,
+		[NidPp] = @NidPp,
+		[Birthdate] = @Birthdate,
+		[GenderId] = @GenderId,
+		[AtollId] = @AtollId,
+		[CountryId] = @CountryId,
+		[Address] = @Address,
+		[PhoneNumber] = @PhoneNumber
+	WHERE [Id] = @Id;
+END
