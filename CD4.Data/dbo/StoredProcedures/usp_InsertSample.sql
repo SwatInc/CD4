76f1716e02d2e7a309a53ef58ee3a4f391b17982
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-17 23:58:26 +0500

    Added stored procedure for insert data row to dbo.Sample.

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertSample.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertSample]
+	@Cin varchar(50),
+	@AnalysisRequestId int,
+	@SiteId int,
+	@CollectionDate char(8),
+	@ReceivedDate char(8)
+AS
+BEGIN
+SET NOCOUNT ON;
+	INSERT INTO [dbo].[Sample]([Cin], [AnalysisRequestId], [SiteId], [CollectionDate], [ReceivedDate])
+	VALUES (@Cin, @AnalysisRequestId, @SiteId, @CollectionDate, @ReceivedDate);
+END
