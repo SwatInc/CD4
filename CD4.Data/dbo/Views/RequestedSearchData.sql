7400af70294f6a7084d6d9376361dc60c615a271
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-22 05:02:50 +0500

    Readded a view which was deleted mistakenly

---
+++ CD4.Data/dbo/Views/RequestedSearchData.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[RequestSearchData]
+	AS
+SELECT 
+	[S].[Cin],[S].[SiteId], [S].[CollectionDate],[S].[ReceivedDate],
+	[P].[NidPp],[P].[FullName],[P].[GenderId],[P].[PhoneNumber],[P].[Birthdate],[P].[Address],
+	[A].[Atoll],[A].[Island], [P].[CountryId]
+FROM [dbo].[Sample] [S]
+INNER JOIN [dbo].[AnalysisRequest] [AR] ON [S].[AnalysisRequestId] = [AR].[Id]
+INNER JOIN [dbo].[Patient] [P] ON [AR].[PatientId] = [P].[Id]
+INNER JOIN [dbo].[Atoll] [A] ON [P].[AtollId] = [A].[Id];
+GO
