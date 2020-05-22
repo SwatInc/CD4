16ca135ee64eb9a422963879136cc006bdd07cba
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-22 05:34:04 +0500

    Added models required to fetch complete request by providing CIN. RequestSearchDataModel and ClinicalDetailIdsCsvModel

---
+++ CD4.DataLibrary/Models/ClinicalDetailIdsCsvModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace CD4.DataLibrary.Models
+{
+    class ClinicalDetailIdsCsvModel
+    {
+        private string csvClinicalDetails;
+        private int[] clinicalDetailIds { get; set; }
+
+        public string CsvClinicalDetails
+        {
+            get => csvClinicalDetails; set
+            {
+                csvClinicalDetails = value;
+                InitializeClinicalDetailIdsArray();
+            }
+        }
+        public int[] GetClinicalDetailIds()
+        {
+            return clinicalDetailIds;
+        }
+        private void InitializeClinicalDetailIdsArray()
+        {
+            clinicalDetailIds = csvClinicalDetails.Split(',')
+                .Select(id => int.Parse(id)).ToArray();
+        }
+
+    }
+}
---
+++ CD4.DataLibrary/Models/RequestSearchDataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.Models
+{
+    public class RequestSearchDataModel
+    {
+        public string Cin { get; set; }
+        public int SiteId { get; set; }
+        public DateTime? CollectionDate { get; set; }
+        public DateTime? ReceivedDate { get; set; }
+        public string NidPp { get; set; }
+        public string Fullname { get; set; }
+        public int GenderId { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string Address { get; set; }
+        public string Atoll { get; set; }
+        public string Island { get; set; }
+        public int CountryId { get; set; }
+
+    }
+}
