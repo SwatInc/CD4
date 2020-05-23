2439470a3295e4e789ab232ab7b86aa01ca4d774
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-24 00:27:36 +0500

    Refactoring

--- CD4.Data/dbo/Tables/Sample.sql
+++ CD4.Data/dbo/Tables/Sample.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Sample]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Id] INT NOT NULL IDENTITY, 
     [Cin] VARCHAR(50) NOT NULL,
     [AnalysisRequestId] INT NOT NULL , 
     [SiteId] INT NOT NULL DEFAULT 0, -- 0 is UNKNOWN on SITES TABLE
@@ -9,4 +9,5 @@
 
     CONSTRAINT [AK_Sample_Cin] UNIQUE ([Cin]), 
     CONSTRAINT [FK_Sample_Sites] FOREIGN KEY ([SiteId]) REFERENCES [dbo].[Sites]([Id]), 
-    CONSTRAINT [FK_Sample_ToTable] FOREIGN KEY ([AnalysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]))
+    CONSTRAINT [FK_Sample_ToTable] FOREIGN KEY ([AnalysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]), 
+    CONSTRAINT [PK_Sample] PRIMARY KEY ([Id]))
