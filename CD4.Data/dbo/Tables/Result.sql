09eb82b277aa335644b3803fe8db16bba3b75fe8
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-24 00:27:05 +0500

    Created an index on sample cin to increase select performance

--- CD4.Data/dbo/Tables/Result.sql
+++ CD4.Data/dbo/Tables/Result.sql
@@ -1,9 +1,13 @@
 ﻿CREATE TABLE [dbo].[Result]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Id] INT NOT NULL IDENTITY, 
     [Sample_Cin] VARCHAR(50) NOT NULL,
     [TestId] INT NOT NULL, 
     [Result] VARCHAR(50) NULL, 
     [ResultDate] DATE NULL, --if result not null, get system date GETDATE()   
     CONSTRAINT [FK_Result_Test] FOREIGN KEY ([TestId]) REFERENCES [dbo].[Test]([Id]), 
-    CONSTRAINT [FK_Result_Sample] FOREIGN KEY ([Sample_Cin]) REFERENCES [dbo].[Sample]([Cin]))
+    CONSTRAINT [FK_Result_Sample] FOREIGN KEY ([Sample_Cin]) REFERENCES [dbo].[Sample]([Cin]), 
+    CONSTRAINT [PK_Id_Cin] PRIMARY KEY ([Id],[Sample_Cin]))
+GO
+
+CREATE INDEX [IX_Result_Column] ON [dbo].[Result] ([Sample_Cin])
