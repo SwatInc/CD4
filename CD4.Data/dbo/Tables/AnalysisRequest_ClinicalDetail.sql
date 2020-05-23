44fb6b92bb577ed24c8368217c50168c976b3203
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-24 00:26:17 +0500

    Added a unique key index to [AnalysisRequestId],[ClinicalDetailsId] to increase query performance

--- CD4.Data/dbo/Tables/AnalysisRequest_ClinicalDetail.sql
+++ CD4.Data/dbo/Tables/AnalysisRequest_ClinicalDetail.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[AnalysisRequest_ClinicalDetail]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Id] INT NOT NULL IDENTITY, 
     [AnalysisRequestId] INT NOT NULL, 
     [ClinicalDetailsId] INT NOT NULL, 
     CONSTRAINT [FK_AnalysisRequest_ClinicalDetail_AnalysisRequest] FOREIGN KEY ([AnalysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]), 
-    CONSTRAINT [FK_AnalysisRequest_ClinicalDetail_ClinicalDetails] FOREIGN KEY ([ClinicalDetailsId]) REFERENCES [dbo].[ClinicalDetail]([Id])
+    CONSTRAINT [FK_AnalysisRequest_ClinicalDetail_ClinicalDetails] FOREIGN KEY ([ClinicalDetailsId]) REFERENCES [dbo].[ClinicalDetail]([Id]), 
+    CONSTRAINT [PK_AnalysisRequest_ClinicalDetail] PRIMARY KEY ([Id]), 
+    CONSTRAINT [AK_AnalysisRequest_ClinicalDetail_Column] UNIQUE ([AnalysisRequestId],[ClinicalDetailsId])
 )
