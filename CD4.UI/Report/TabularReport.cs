28a7eef991be9c93e2829a240e7ef2b5e676abd0
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-05-28 07:20:32 +0500

    Added a blank report to UI

---
+++ CD4.UI/Report/TabularReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace CD4.UI.Report
+{
+    public partial class TabularReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public TabularReport()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
