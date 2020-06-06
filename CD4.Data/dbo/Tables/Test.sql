e3fd1d89d299751a16544575c671ea404b22a49c
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-06-06 18:40:19 +0500

    Added fields discipline and Unit to dbo.Test

--- CD4.Data/dbo/Tables/Test.sql
+++ CD4.Data/dbo/Tables/Test.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[Test]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Discipline] VARCHAR(50) NULL,
     [Description] VARCHAR(50) NOT NULL, 
     [ResultDataTypeId] INT NOT NULL, 
-    [Mask] VARCHAR(50) NOT NULL, 
+    [Mask] VARCHAR(50) NOT NULL,
+    [Unit] VARCHAR(10) NULL,
     [Reportable] BIT NOT NULL, 
     CONSTRAINT [FK_Test_ResultDataType] FOREIGN KEY ([ResultDataTypeId]) REFERENCES [dbo].[ResultDataType]([Id]), 
     CONSTRAINT [AK_Test_Description] UNIQUE ([Description])
