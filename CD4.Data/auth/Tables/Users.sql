54ec5f349d95b0136f7cdd86cea81d776f3691e2
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-06-17 18:25:17 +0500

    Refactoring identity related tables

--- CD4.Data/auth/Tables/Users.sql
+++ CD4.Data/auth/Tables/Users.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[AspNetUsers](
+﻿CREATE TABLE [dbo].[Users](
 	[Id] [nvarchar](450) NOT NULL,
 	[UserName] [nvarchar](256) NULL,
 	[NormalizedUserName] [nvarchar](256) NULL,
@@ -14,7 +14,7 @@
 	[LockoutEnd] [datetimeoffset](7) NULL,
 	[LockoutEnabled] [bit] NOT NULL,
 	[AccessFailedCount] [int] NOT NULL,
- CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
