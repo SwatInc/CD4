e9f42f95e445879900565328c31da2731b1e9276
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-06-19 11:42:53 +0500

    Added stored procedure to get username and hash for specified username

---
+++ CD4.Data/auth/StoredProcedures/usp_GetHashForUsername.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetHashForUsername]
+	@username varchar(256)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT [U].[UserName], [U].[PasswordHash]
+	FROM [dbo].[Users] [U]
+	WHERE [U].[UserName] = @username;
+END
