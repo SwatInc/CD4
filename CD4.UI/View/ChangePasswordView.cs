a548188d91af8f850f60e14a509b3c60f0575480
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-06-20 09:05:22 +0500

    refactoring

--- CD4.UI/View/ChangePasswordView.cs
+++ CD4.UI/View/ChangePasswordView.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
 using CD4.UI.Library.ViewModel;
 
 namespace CD4.UI.View
 {
-    public partial class ChangePasswordView : DevExpress.XtraEditors.XtraForm
+    public partial class ChangePasswordView : XtraForm
     {
         private readonly IChangePasswordViewModel viewModel;
 
