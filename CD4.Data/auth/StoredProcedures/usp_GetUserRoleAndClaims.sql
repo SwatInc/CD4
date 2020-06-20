ae66286a8bf37c8f4d4b4d51016262d474915844
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-06-20 19:10:33 +0500

    Refactoring the sql query

--- CD4.Data/auth/StoredProcedures/usp_GetUserRoleAndClaims.sql
+++ CD4.Data/auth/StoredProcedures/usp_GetUserRoleAndClaims.sql
@@ -2,17 +2,21 @@
 	@Username varchar(256)
 AS
 BEGIN
+
+	DECLARE @Claims varchar(max);
+
+		--claims for all roles
+	SELECT @Claims  = STRING_AGG([RC].[ClaimValue],',')
+	FROM [dbo].[Users] [U]
+	INNER JOIN [dbo].[UserRoles] [UR] ON [U].[Id] = [UR].[UserId]
+	INNER JOIN [dbo].[RoleClaims] [RC] ON [RC].[RoleId] = [UR].[RoleId]
+	WHERE [U].[Username] = @Username;
+
 	--role, username, fullname, 
-	SELECT [R].[Name] AS [UserRole], [U].[UserName], [U].[Fullname]
+	SELECT [R].[Name] AS [UserRole], [U].[UserName], [U].[Fullname], @Claims AS [Claims]
 	FROM [dbo].[Users] [U]
 	INNER JOIN [dbo].[UserRoles] [UR] ON [U].[Id] = [UR].[UserId]
 	INNER JOIN [dbo].[Roles] [R] ON [R].[Id] = [UR].[RoleId]
 	WHERE [U].[Username] = @Username;
 
-	--claims for all roles
-	SELECT [RC].[ClaimValue] AS [Claims]
-	FROM [dbo].[Users] [U]
-	INNER JOIN [dbo].[UserRoles] [UR] ON [U].[Id] = [UR].[UserId]
-	INNER JOIN [dbo].[RoleClaims] [RC] ON [RC].[RoleId] = [UR].[RoleId]
-	WHERE [U].[Username] = @Username;;
 END
