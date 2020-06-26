98f06442a9f3911869f469e593eb0b1c54fe97e9
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-06-26 20:56:49 +0500

    #15 Added comments

--- CD4.UI/Report/SeventyFiveMillimeterTubeLabel.cs
+++ CD4.UI/Report/SeventyFiveMillimeterTubeLabel.cs
@@ -6,6 +6,9 @@ using DevExpress.XtraReports.UI;
 
 namespace CD4.UI.Report
 {
+    /// <summary>
+    /// The is the barcode label that will be printed for 75mm standard tubes
+    /// </summary>
     public partial class SeventyFiveMillimeterTubeLabel : DevExpress.XtraReports.UI.XtraReport
     {
         public SeventyFiveMillimeterTubeLabel()
