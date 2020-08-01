e2eaa0e07c710e5b17eac59012c895e78645525b
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-08-01 16:14:35 +0500

    Removed tracking fields from Sample, request and results tables

--- CD4.Data/dbo/Tables/Result.sql
+++ CD4.Data/dbo/Tables/Result.sql
@@ -4,8 +4,7 @@
     [Sample_Cin] VARCHAR(50) NOT NULL,
     [TestId] INT NOT NULL, 
     [Result] VARCHAR(50) NULL,
-    [StatusId] INT NOT NULL DEFAULT 1,
-    [ResultDate] DATE NULL, --if result not null, get system date GETDATE()   
+    [StatusId] INT NOT NULL DEFAULT 1,  
     CONSTRAINT [FK_Result_Test] FOREIGN KEY ([TestId]) REFERENCES [dbo].[Test]([Id]), 
     CONSTRAINT [FK_Result_Sample] FOREIGN KEY ([Sample_Cin]) REFERENCES [dbo].[Sample]([Cin]), 
     CONSTRAINT [PK_Id_Cin] PRIMARY KEY ([Id],[Sample_Cin]), 
--- CD4.Data/dbo/Tables/Sample.sql
+++ CD4.Data/dbo/Tables/Sample.sql
@@ -5,9 +5,6 @@
     [StatusId] INT NOT NULL,
     [AnalysisRequestId] INT NOT NULL , 
     [SiteId] INT NOT NULL DEFAULT 1, -- 0 is UNKNOWN on SITES TABLE
-    [CollectionDate] DATE NULL , 
-    [ReceivedDate] DATE NOT NULL DEFAULT GETDATE(), 
-
     CONSTRAINT [AK_Sample_Cin] UNIQUE ([Cin]), 
     CONSTRAINT [FK_Sample_Sites] FOREIGN KEY ([SiteId]) REFERENCES [dbo].[Sites]([Id]), 
     CONSTRAINT [FK_Sample_AnalysisRequest] FOREIGN KEY ([AnalysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]), 
