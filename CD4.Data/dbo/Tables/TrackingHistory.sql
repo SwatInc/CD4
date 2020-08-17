3659ff31df8cc5f78ab0077fef826b646c83fea5
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-08-18 02:43:27 +0500

    Corrected a spelling mistake

--- CD4.Data/dbo/Tables/TrackingHistory.sql
+++ CD4.Data/dbo/Tables/TrackingHistory.sql
@@ -2,12 +2,14 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[TrackingType] INT NOT NULL,
-	[AnslysisRequestId] INT,
+	[AnalysisRequestId] INT,
 	[SampleCin] varchar(50),
 	[ResultId] int,
 	[StatusId] INT NOT NULL,
+	[UsersId] INT NOT NULL,
 	[TimeStamp] DATETIMEOFFSET DEFAULT GETDATE(), 
-    CONSTRAINT [FK_TrackingHistory_AnalysisRequest] FOREIGN KEY ([AnslysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]),
+    CONSTRAINT [FK_TrackingHistory_AnalysisRequest] FOREIGN KEY ([AnalysisRequestId]) REFERENCES [dbo].[AnalysisRequest]([Id]),
     CONSTRAINT [FK_TrackingHistory_Sample] FOREIGN KEY ([SampleCin]) REFERENCES [dbo].[Sample]([Cin]),
-    CONSTRAINT [FK_TrackingHistory_Result] FOREIGN KEY ([ResultId]) REFERENCES [dbo].[Result]([Id])
+    CONSTRAINT [FK_TrackingHistory_Result] FOREIGN KEY ([ResultId]) REFERENCES [dbo].[Result]([Id]), 
+    CONSTRAINT [FK_TrackingHistory_Users] FOREIGN KEY ([UsersId]) REFERENCES [dbo].[Users]([Id])
 )
