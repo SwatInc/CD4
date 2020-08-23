014f44111e7ea43e898b68c674163af9e0017136
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-08-23 14:21:09 +0500

    #20 Preliminary tables for implementation of reference ranges.

---
+++ CD4.Data/dbo/Tables/ReferenceData.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ReferenceData]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ReferenceTypeId] INT NOT NULL,
+	[HighLimitValue] DECIMAL(10, 8) NOT NULL,
+	[LowLimitvalue] DECIMAL(10, 8) NOT NULL
+)
---
+++ CD4.Data/dbo/Tables/ReferenceRange.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ReferenceRange]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[TestId] INT NOT NULL,
+	[GenderId] INT NOT NULL,
+	[FromAgeDays] INT NOT NULL,
+	[ToAgeDays] INT NOT NULL,
+	[DeltaValidityIntervalDays] INT NOT NULL,
+	[BiasFactor] INT NOT NULL,
+	[RaceId] INT NOT NULL,
+	[ClinicalDetailsId] INT NOT NULL
+)
