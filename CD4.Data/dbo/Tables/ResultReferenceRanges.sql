60e5dacaae4976f711ef10f78b1c86358b8de875
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-08-23 15:46:41 +0500

    #20 Added ResultReferenceRanges table. This table allows the database to maintain previous reference ranges for validated tests even after reference ranges are changed later

---
+++ CD4.Data/dbo/Tables/ResultReferenceRanges.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[ResultReferenceRanges]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ResultId] INT NOT NULL, 
+    [NormalHighLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1, 
+    [NormalLowLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [AttentionHighLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1, 
+    [AttentionLowLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [PathologyHighLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1, 
+    [PathologyLowLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [AbsoluteDeltaLowLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [AbsoluteDeltaHighLimit] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [RelativeDeltaLow] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [RelativeDeltaHigh] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityAbsoluteLow_DeltaLow] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityAbsoluteLow_DeltaHigh] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityAbsoluteHigh_DeltaLow] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityAbsoluteHigh_DeltaHigh] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityRelativeLow_DeltaLow] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityRelativeLow_DeltaHigh] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityRelativeHigh_DeltaLow] DECIMAL(10, 8) NOT NULL DEFAULT -1,
+    [OutOfNormalityRelativeHigh_DeltaHigh] DECIMAL(10, 8) NOT NULL DEFAULT -1, 
+    [DeltaValidityDays] INT NOT NULL DEFAULT 0, 
+    [BiasFactor] INT NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_ResultReferenceRanges_ResultId] FOREIGN KEY ([ResultId]) REFERENCES [dbo].[Result]([Id]),
+
+    )
