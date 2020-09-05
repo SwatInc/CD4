5ae35a40f354696ad7f4c5a5a8b7f9e7c4b04ddc
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-09-05 12:10:07 +0500

    #52 implemented data layer code to fetch audit trail by Cin

---
+++ CD4.DataLibrary/Models/AuditTrailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.Models
+{
+    public class AuditTrailModel
+    {
+        public DateTimeOffset CreatedAt { get; set; }
+        public string Details { get; set; }
+        public string Cin { get; set; }
+    }
+}
