935ea35cdb9016237d2169f54d9a488261f761c6
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-09-06 23:17:01 +0500

    Toggles audit trail and selected tests for samples

---
+++ CD4.UI/UiSpecificModels/GridColumnModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.UI.UiSpecificModels
+{
+    public class GridColumnModel
+    {
+        public string Caption { get; set; }
+        public string FieldName { get; set; }
+        public bool AllowEdit { get; set; }
+        public string Name { get; set; }
+        public bool Visible { get; set; }
+        public int VisibleIndex { get; set; }
+        public int Width { get; set; }
+    }
+}
