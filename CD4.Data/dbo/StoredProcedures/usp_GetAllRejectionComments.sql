4fb2be1130b23a0ffb15d87f226643053001fd89
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-09-29 23:00:39 +0500

    Added stored procedure and dataaccess code to fetch all comments by comment Id

---
+++ CD4.Data/dbo/StoredProcedures/usp_GetAllRejectionComments.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetAllCommentsByTypeId]
+ @CommentTypeId int
+AS
+BEGIN
+	SELECT [CL].[Id], [CL].[Description]
+	FROM [dbo].[CommentList] [CL]
+	INNER JOIN [dbo].[CommentList_CommentType] [CCT] ON [CL].[Id] = [CCT].[CommentListId]
+	INNER JOIN [dbo].[CommentType] [CT] ON [CCT].[CommentTypeId] = [CT].[Id]
+	WHERE [CT].[id] = @CommentTypeId AND 
+		  [CCT].[IsActive] = 1 AND
+		  [CL].[IsActive] = 1;
+END
---
+++ CD4.DataLibrary/DataAccess/CommentsDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using CD4.DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.DataAccess
+{
+    public class CommentsDataAccess: DataAccessBase
+    {
+        public async Task<List<CommentsSelectionModel>> GetAllCommentsByTypeId(int commentTypeId)
+        {
+            var storedProcedure = "[dbo].[usp_GetAllCommentsByTypeId]";
+            var parameter = new { CommentTypeId = commentTypeId };
+            return await LoadDataWithParameterAsync<CommentsSelectionModel, dynamic>
+                (storedProcedure, parameter);
+        }
+    }
+}
