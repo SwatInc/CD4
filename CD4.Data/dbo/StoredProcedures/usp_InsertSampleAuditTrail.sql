54289f0623a8aeb2efc709fd62b95011b76c117a
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-10-09 12:25:31 +0500

    Fixed a bug in the stored procedure to properly output inserted Id instead of selecting it

--- CD4.Data/dbo/StoredProcedures/usp_InsertSampleAuditTrail.sql
+++ CD4.Data/dbo/StoredProcedures/usp_InsertSampleAuditTrail.sql
@@ -1,13 +1,15 @@
 ﻿CREATE PROCEDURE [dbo].[usp_InsertSampleAuditTrail]
     @Cin varchar(50),
     @StatusId int,
-    @Details varchar(2000)
+    @Details varchar(2000),
+	@InsertedId int output
 AS
 BEGIN
     SET NOCOUNT ON;
     SET XACT_ABORT ON;
 
     DECLARE @SampleAuditTypeId int;
+	DECLARE @InsertedData TABLE ([InsertedId] int not null);
 
     SELECT @SampleAuditTypeId = [Id]
     FROM [dbo].[AuditTypes]
@@ -23,11 +25,14 @@ BEGIN
             [StatusId],
             [Details]
         )
-        OUTPUT INSERTED.[Id]
+        OUTPUT INSERTED.[Id] INTO @InsertedData
         VALUES
         (@SampleAuditTypeId, @Cin, 7, @Details);
 
         COMMIT TRANSACTION;
+
+		SELECT TOP(1) @InsertedId = [InsertedId] FROM @InsertedData;
+
     END TRY
     BEGIN CATCH
         ROLLBACK TRANSACTION;
