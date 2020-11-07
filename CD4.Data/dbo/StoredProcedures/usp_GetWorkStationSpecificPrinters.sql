61ae2b3a9fa7d9784f461e7338c1c028869880b9
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-11-08 04:29:31 +0500

    Added stored procedures to GetBarcodeDetails and get WorkStationSpecificPrinters

---
+++ CD4.Data/dbo/StoredProcedures/usp_GetWorkStationSpecificPrinters.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetWorkStationSpecificPrinters]
+	@WorkStationName varchar(100)
+AS
+BEGIN
+	SELECT [P].[Description] AS [PrinterName], [PT].[Id] AS [PrinterType]
+	FROM [dbo].[Printers] [P]
+	INNER JOIN [dbo].[WorkStationPrinters] [WSP] ON [P].[Id] = [WSP].[PrinterId]
+	INNER JOIN [dbo].[WorkStations] [WS] ON [WSP].[WorkStationId] = [WS].[Id]
+	INNER JOIN [dbo].[PrinterTypes] [PT] ON [WSP].[PrinterTypeId] = [PT].[Id]
+	WHERE [WS].[Description] = @WorkStationName;
+END
