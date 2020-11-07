21778e02daba46c2566d7b6c53018053e151b5a3
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-11-08 04:31:29 +0500

    Refactored. Changed a misleading FK name from PrinterType to PrinterTypeId

--- CD4.Data/dbo/Tables/WorkStationPrinters.sql
+++ CD4.Data/dbo/Tables/WorkStationPrinters.sql
@@ -3,10 +3,10 @@
 	[Id] INT NOT NULL IDENTITY,
 	[WorkStationId] int NOT NULL,
 	[PrinterId] int NOT NULL, 
-	[PrinterType] int NOT NULL, 
+	[PrinterTypeId] int NOT NULL, 
     CONSTRAINT [PK_WorkStationPrinters] PRIMARY KEY ([Id]), 
     CONSTRAINT [FK_WorkStationPrinters_WorkStations] FOREIGN KEY ([WorkStationId]) REFERENCES [WorkStations]([Id]),
     CONSTRAINT [FK_WorkStationPrinters_Printers] FOREIGN KEY ([PrinterId]) REFERENCES [Printers]([Id]),
-	CONSTRAINT [FK_WorkStationPrinters_PrinterTypes] FOREIGN KEY ([PrinterType]) REFERENCES [PrinterTypes]([Id]),
+	CONSTRAINT [FK_WorkStationPrinters_PrinterTypes] FOREIGN KEY ([PrinterTypeId]) REFERENCES [PrinterTypes]([Id]),
 
 )
