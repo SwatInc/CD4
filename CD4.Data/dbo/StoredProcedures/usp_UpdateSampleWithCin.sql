93026a0beaa4b8c84dd0ca982107af92bb9544bb
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-11-10 07:52:12 +0500

    Enters a sample collection record to tracking history without marking collected on Sample tracking or TestTracking. The sample and test status stays as registered on UI unless barcode is printed

--- CD4.Data/dbo/StoredProcedures/usp_UpdateSampleWithCin.sql
+++ CD4.Data/dbo/StoredProcedures/usp_UpdateSampleWithCin.sql
@@ -13,6 +13,15 @@ DECLARE @ReturnValue bit = 0;
 				
 				DECLARE @AuditTypeIdSample int;
 				DECLARE @CurrentStatus int;
+				DECLARE @IsSampleCollected int;
+				DECLARE @IsSampleAccepted int;
+				DECLARE @SampleAuditType int  = 2;
+				DECLARE @CollectedStatusId int = 2;
+				DECLARE @AcceptedStatusId int = 2;
+
+				-- check whether sample is accepted / collected
+				SELECT @IsSampleCollected = COUNT([Id]) FROM [dbo].[TrackingHistory] WHERE [SampleCin] = @Cin AND [StatusId] = 2;
+				SELECT @IsSampleAccepted = COUNT([Id]) FROM [dbo].[TrackingHistory] WHERE [SampleCin] = @Cin AND [StatusId] = 3;
 
 				--get current
 				SELECT @CurrentStatus = [StatusId] FROM [dbo].[SampleTracking] WHERE [SampleCin] = @Cin;
@@ -23,13 +32,24 @@ DECLARE @ReturnValue bit = 0;
 				WHERE [Cin] = @Cin;
 
 				-- insert / update sample collected date
+				INSERT INTO [dbo].[TrackingHistory]([TrackingType],[SampleCin],[StatusId],[UsersId])
+				SELECT @SampleAuditType,@Cin,@CollectedStatusId,1
+				WHERE @IsSampleCollected = 0;
+
 				UPDATE [dbo].[TrackingHistory]
 				SET [TimeStamp] = @CollectionDate
-				WHERE [SampleCin] = @Cin AND [StatusId] = 2
+				WHERE [SampleCin] = @Cin AND [StatusId] = 2;
+
 				-- insert / update sample received date
+				INSERT INTO [dbo].[TrackingHistory]([TrackingType],[SampleCin],[StatusId],[UsersId])
+				SELECT @SampleAuditType,@Cin,@AcceptedStatusId,1
+				WHERE @AcceptedStatusId = 0;
+
 				UPDATE [dbo].[TrackingHistory]
-				SET [TimeStamp] = @CollectionDate
-				WHERE [SampleCin] = @Cin AND [StatusId] = 3
+				SET [TimeStamp] = @ReceivedDate
+				WHERE [SampleCin] = @Cin AND [StatusId] = 3;
+
+
 
 				-- AUDIT
 				SELECT @AuditTypeIdSample = [Id] FROM [dbo].[AuditTypes] WHERE [Description] = 'Sample';
