b64fd250ea5045122deb6de0543f5f9162cd9466
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-11-26 16:57:05 +0500

    Added dataaccess layer code to read and update global settings

---
+++ CD4.DataLibrary/DataAccess/GlobalSettingsDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using CD4.DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.DataAccess
+{
+    public class GlobalSettingsDataAccess : DataAccessBase, IGlobalSettingsDataAccess
+    {
+        public async Task<GlobalSettingsModel> ReadAllGlobalSettingsAsync()
+        {
+            var storedProcedure = "[dbo].[usp_ReadGlobalSettings]";
+            return  (await LoadDataAsync<GlobalSettingsModel>(storedProcedure)).FirstOrDefault();
+        }
+
+        public async Task UpdateGlobalSetting(GlobalSettingsModel globalSettings)
+        {
+            var storedProcedure = "[dbo].[usp_UpdateGlobalSettings]";
+            _ = await LoadDataAsync<dynamic>(storedProcedure);
+        }
+    }
+}
---
+++ CD4.DataLibrary/Models/GlobalSettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.Models
+{
+    public class GlobalSettingsModel
+    {
+        public int Id { get; set; }
+        public bool VerifyNidPpOnOrder { get; set; }
+    }
+}
