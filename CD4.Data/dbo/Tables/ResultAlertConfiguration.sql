919dc8691ced587e34d9b6ad0e01173a9e411066
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-12-11 12:48:19 +0500

    Added database layer code to handle result alert massages.

---
+++ CD4.Data/dbo/Tables/ResultAlertConfiguration.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ResultAlertConfiguration]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[TestId] INT NOT NULL,
+	[Result] VARCHAR(100) NOT NULL,
+	[AlertMessage] VARCHAR(100) NOT NULL,
+	[Operator] VARCHAR(2) NOT NULL, 
+    CONSTRAINT [FK_ResultAlertConfiguration_Test] FOREIGN KEY ([TestId]) REFERENCES [dbo].[Test]([Id])
+)
