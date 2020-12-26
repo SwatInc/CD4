ac33841b571dbbd0d77ff7a7c18a37e9460912b7
Ibrahim Hussain <ibrahim.hucyn@outlook.com>
2020-12-26 10:38:28 +0500

    Updated datalayer models to include username

---
+++ CD4.DataLibrary/Models/SampleNotesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CD4.DataLibrary.Models
+{
+    public class SampleNotesModel
+    {
+        public string CIN { get; set; }
+        public string Note { get; set; }
+        public bool IsAttended { get; set; }
+        public int UserId { get; set; }
+    }
+}
