ba1ada5477e4f3233722c2aaa1e6972e02d41c74
Ibrahim Hussain <ibrahim.hucyn@outlook.com>
2020-12-26 22:27:29 +0500

    Added timestamp to the notes table

--- CD4.Data/dbo/Tables/SampleNotes.sql
+++ CD4.Data/dbo/Tables/SampleNotes.sql
@@ -5,6 +5,7 @@
     [Note] VARCHAR(200) NOT NULL, 
     [IsAttended] BIT NOT NULL DEFAULT 0, 
     [UserId] INT NOT NULL, 
+    [TimeStamp] DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET( ) , 
     CONSTRAINT [FK_SampleNotes_Sample] FOREIGN KEY ([Cin]) REFERENCES [dbo].[Sample]([Cin]), 
     CONSTRAINT [FK_SampleNotes_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users]([Id])
 )
