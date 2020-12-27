00a912528e615f7505947e56983f6d7d5a9e36a6
Ibrahim Hussain <ibrahim.hucyn@outlook.com>
2020-12-27 23:47:52 +0500

    Added stored procedures to get sample notes, insert new notes and update attended status

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertSampleNoteByCin.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertSampleNoteByCin]
+	@Cin varchar(50),
+	@Note varchar(200),
+	@UserId int
+AS
+BEGIN
+	INSERT INTO [dbo].[SampleNotes] ([Cin],[Note],[UserId])
+	OUTPUT INSERTED.[Id]
+	     , INSERTED.[Cin]
+		 , INSERTED.[Note]
+		 , INSERTED.[IsAttended]
+		 , INSERTED.[UserId]
+		 , INSERTED.[TimeStamp]
+	VALUES
+	(@Cin,@Note,@UserId);
+END
---
+++ CD4.Data/dbo/StoredProcedures/usp_UpdateSampleNoteAttendedStatus.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[usp_UpdateSampleNoteAttendedStatus]
+	@NoteId int,
+	@IsAttended bit
+AS
+BEGIN
+	UPDATE [dbo].[SampleNotes]
+	SET [IsAttended] = @IsAttended
+	WHERE [Id] = @NoteId;
+END
