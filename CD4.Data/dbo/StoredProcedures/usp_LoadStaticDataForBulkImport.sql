65c418f4baffed4cb7720a36447a8b660395eda7
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-01-11 22:53:41 +0500

    Added basic stored procedure to fetch data for bulk import

---
+++ CD4.Data/dbo/StoredProcedures/usp_LoadStaticDataForBulkImport.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[usp_LoadStaticDataForBulkImport]
+AS
+BEGIN
+	-- load genders list
+    SELECT [Id],[Gender] FROM [dbo].[Gender];
+	-- load atoll island data
+    SELECT [Id],[Atoll],[Island] FROM [dbo].[Atoll];
+	-- load Sites data
+    SELECT [Id],[Name] FROM [dbo].[Sites];
+	-- load all nationalities
+    SELECT [Id],[Country] FROM [dbo].[Country];
+	-- load all clinical details data
+    SELECT [Id],[Detail] FROM [dbo].[ClinicalDetail];
+	-- load all tests data
+    SELECT [Id],[Description] FROM [dbo].[Test];
+END
