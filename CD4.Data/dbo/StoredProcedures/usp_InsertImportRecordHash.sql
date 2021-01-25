5be21e3dee0e9053c462b8d27786643dd87a9a14
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-01-25 20:35:25 +0500

    Added and edited stored procedure to get Cins for hashes, insert import hashes and updated bulk imported hash

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertImportRecordHash.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertImportRecordHash]
+	@Cin varchar(50),
+	@Hash int
+AS
+BEGIN
+	INSERT INTO [dbo].[BulkImportedHash]([Cin],[Hash]) 
+	VALUES
+	(@Cin, @Hash)
+END
---
+++ CD4.Data/dbo/Tables/BulkImportedHash.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[BulkImportedHash]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Cin] VARCHAR(50) NOT NULL,
+	[Hash] INT NOT NULL,
+    CONSTRAINT [AK_BulkImportedHash_Hash] UNIQUE ([Hash])
+)
