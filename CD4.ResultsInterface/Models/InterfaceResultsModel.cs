a2d232657c5e8a096657bd60fcde3983de38b286
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-02-05 15:04:49 +0500

    Added required models

---
+++ CD4.ResultsInterface/Models/InterfaceResultsModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.ResultsInterface.Models
+{
+    public class InterfaceResultsModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Lookup
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+        public InterfaceResultsModel()
+        {
+            Measurements = new List<MeasurementValues>();
+            InstrumentId = new InstrumentIdentification();
+        }
+        public string SampleId { get; set; }
+        public string Dilution { get; set; }
+        public string ReagentLot { get; set; }
+        public string ReagentSerialNumber { get; set; }
+        public string ControlLotNumber { get; set; }
+        public string ResultType { get; set; }
+        public string BatchId { get; set; }
+        public List<MeasurementValues> Measurements { get; set; }
+        public string Unit { get; set; }
+        public string ReferenceRange { get; set; }
+        public string AbnormalFlags { get; set; }
+        public string NatureOfAbnormality { get; set; }
+        public ResultStatus ResultStatus { get; set; }
+        public string OperatorIdentification { get; set; }
+        public string StartDateTime { get; set; }
+        public string CompletedDateTime { get; set; }
+        public InstrumentIdentification InstrumentId { get; set; }
+
+    }
+}
--- CD4.ResultsInterface/Worker.cs
+++ CD4.ResultsInterface/Worker.cs
@@ -1,7 +1,9 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,10 +13,11 @@ namespace CD4.ResultsInterface
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
+        private readonly IConfiguration _configuration;
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
@@ -26,8 +29,8 @@ namespace CD4.ResultsInterface
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Worker running at: {time}: {data}", DateTimeOffset.Now, _configuration.GetValue<string>("IncomingDirectory"));
+                await Task.Delay(60*1000, stoppingToken);
             }
         }
     }
