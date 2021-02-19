96ef1204857a9ed62aadc3f905dc584688f28b16
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-02-19 13:49:50 +0500

    Added user defined table to hold the multiple cins for fetching barcode data. Added a stored procedure which utilizes the UDT to return barcode data for multiple samples

---
+++ CD4.Data/dbo/StoredProcedures/usp_GetBarcodeDetailsForMultipleSamples.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetBarcodeDetailsForMultipleSamples]
+	@SampleCins [dbo].[SampleCinsUDT] READONLY
+AS
+BEGIN
+			SELECT 
+			 DISTINCT([S].[Id]) AS [Seq]
+			,[S].[Cin] AS [AccessionNumber]
+			,[P].[NidPp]
+			,[P].[FullName]
+			,[P].[Birthdate]
+			,[AR].[Age]
+			,[D].[Description] AS [Discipline]
+			,[TH].[TimeStamp] AS [CollectionDate]
+		FROM dbo.[Sample] [S]
+		INNER JOIN [dbo].[AnalysisRequest] [AR] ON [S].[AnalysisRequestId] = [AR].[Id]
+		INNER JOIN [dbo].[Patient] [P] ON [P].[Id] = [AR].[PatientId]
+		INNER JOIN [dbo].[Result] [R] ON [R].[Sample_Cin] = [S].[Cin]
+		INNER JOIN [dbo].[Test] [T] ON [T].[Id] = [R].[TestId]
+		INNER JOIN [dbo].[Discipline] [D] ON [D].[Id] = [T].[DisciplineId]
+		INNER JOIN [dbo].[TrackingHistory] [TH] ON [TH].[SampleCin] = [S].[Cin]
+		WHERE [TH].[StatusId] = 2 AND [TH].[TrackingType] = 2 AND [S].[Cin] IN (SELECT [Cin] FROM @SampleCins) ;
+END
---
+++ CD4.Data/dbo/Types/SampleCinsUDT.sql
@@ -0,0 +1,4 @@
+﻿CREATE TYPE [dbo].[SampleCinsUDT] AS TABLE
+(
+	[Sample_Cin] VARCHAR(50) NOT NULL
+);
