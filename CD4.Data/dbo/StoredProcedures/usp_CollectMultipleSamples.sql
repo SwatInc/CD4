187ed8a5cf97847499983abe7790b2af8f740994
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-02-19 16:08:17 +0500

    A stored procedure to mark multiple samples as collected

---
+++ CD4.Data/dbo/StoredProcedures/usp_CollectMultipleSamples.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[usp_CollectMultipleSamples]
+	@SampleCins  [dbo].[SampleCinsUDT] READONLY,
+	@UserId int
+AS
+BEGIN
+	DECLARE @SampleCin varchar(50);
+    DECLARE @Counter int  = 0;
+    DECLARE @RecordsNumber int;
+
+    SELECT @RecordsNumber = COUNT([Cin]) FROM @SampleCins;
+
+    WHILE @Counter < @RecordsNumber 
+    BEGIN
+        SELECT @SampleCin = [Cin]
+        FROM @SampleCins
+        ORDER BY [Cin]
+        OFFSET @Counter ROWS FETCH NEXT 1 ROWS ONLY;
+    
+        EXECUTE [dbo].[usp_DecideToAndExecuteMarkSampleCollected] 
+        @Cin = @SampleCin,
+        @UserId = @UserId;
+
+        SET @Counter = @Counter + 1;
+    END;
+
+END
