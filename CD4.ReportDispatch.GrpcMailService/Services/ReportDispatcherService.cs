dd215005a1d9d3f0643f2fbcae7f5b6f41ff1995
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-03-17 14:05:50 +0500

    Added rudimentary report dispatcher service

---
+++ CD4.ReportDispatch.GrpcMailService/Services/ReportDispatcherService.cs
@@ -0,0 +1,108 @@
+﻿using CD4.ReportDispatch.SDK.Models;
+using CD4.ReportDispatch.SDK.Services;
+using FluentEmail.Core;
+using FluentEmail.Razor;
+using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CD4.ReportDispatch.GrpcMailService.Services
+{
+    public class ReportDispatcherService : ReportDispatcher.ReportDispatcherBase
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly ISmtpService _smtpService;
+
+        public ReportDispatcherService(ILogger<ReportDispatcherService> logger,
+            IConfiguration configuration,
+            ISmtpService smtpService)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _smtpService = smtpService;
+        }
+        public override async Task<Response> Dispatch
+            (DispatchRequestModel request, ServerCallContext context)
+        {
+            var a  = _configuration.GetValue<string>("Logging:LogLevel:Default");
+            _logger.LogInformation("Get reports for all sample numbers,");
+            _logger.LogWarning(a);
+
+            var sids = "";
+            foreach (var sid in request.SampleIds)
+            {
+                sids += $"\n{sid}";
+            }
+            _logger.LogInformation(sids);
+            _logger.LogInformation($"\nEmail Template: {request.TemplateName} \nTo Email: {request.ToEmail}");
+
+
+            var mailModel = new EmailModel()
+            {
+                Subject = $"CD4 LIMS: COVID-19 PCR Test Results {DateTime.Today:dd.MM.yyyy}",
+                ToAddress = "ibrahim.hucyn@gmail.com",
+                ToDisplayName = "Ibrahim",
+                Template = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "medlab_reportdispatch_template.html"))
+            };
+            try
+            {
+                await ExecuteAsync(_smtpService, await (new AttachmentService()).GetAttachmentsAsync(), mailModel);
+                return new Response() { Status = 200, Message = "All OK" };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return new Response() { Status = 500, Message = ex.Message };
+            }
+            //await ExecuteAsync(new OutlookSmptService(), await (new AttachmentService()).GetAttachmentsAsync(), mailModel);
+
+            //uncomment to send via gmail. have to have the API key in GmailSmptService class
+            //await ExecuteAsync(new GmailSmptService(), await (new AttachmentService()).GetAttachmentsAsync(), mailModel);
+
+            
+        }
+
+        public async Task ExecuteAsync(ISmtpService smtpService, AttachmentCollection attachments, EmailModel emailModel)
+        {
+            Email.DefaultSender = smtpService.GetSmtpSender();
+            Email.DefaultRenderer = new RazorRenderer();
+
+            var maxMailSize = 25;
+
+            if ((attachments.AttachmentSize / (1024 * 1024)) < maxMailSize)
+            {
+                try
+                {
+                    await Task.Delay(3000);
+                    var email = await Email
+                        .From(smtpService.SmtpSettings.FromAddress, "swatincadmin")
+                        .To(emailModel.ToAddress, emailModel.ToDisplayName)
+                        .Subject(emailModel.Subject)
+                        .Attach(attachments.Attachments)
+                        .UsingTemplate(emailModel.Template, emailModel.TemplateModel, true)
+                        .SendAsync();
+
+                    //close all filestreams
+                    foreach (var item in attachments.Attachments)
+                    {
+                        item.Data.Close();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+            else
+            {
+                throw new Exception($"Cannot mail since the mail size exceeds maximum allowed {maxMailSize} MB");
+            }
+
+        }
+    }
+}
--- CD4.ReportDispatch.GrpcMailService/Startup.cs
+++ CD4.ReportDispatch.GrpcMailService/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CD4.ReportDispatch.GrpcMailService.Services;
+using CD4.ReportDispatch.SDK.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,14 +14,14 @@ namespace CD4.ReportDispatch.GrpcMailService
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddLogging();
+            services.AddSingleton(typeof(ISmtpService), typeof(PapercutSmtpService));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,11 +33,11 @@ namespace CD4.ReportDispatch.GrpcMailService
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<ReportDispatcherService>();
 
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client.");
                 });
             });
         }
