378c23922f5219798b4a749a30f995af43c468f8
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-03-22 16:36:47 +0500

    updated config to reflect retry logic

--- CD4.ExcelInterface.QuantStudio5/ViewModels/MainViewModel.cs
+++ CD4.ExcelInterface.QuantStudio5/ViewModels/MainViewModel.cs
@@ -174,9 +174,7 @@ namespace CD4.ExcelInterface.QuantStudio5.ViewModels
             {
                 foreach (var file in _filesToProcess)
                 {
-
-                    FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
-
+                    var fileStream = MultipleTries(()=>new FileStream(file, FileMode.Open, FileAccess.Read));
                     var results = ProcessExcelFile(fileStream);
 
                     //delete the file if it exists
@@ -205,6 +203,34 @@ namespace CD4.ExcelInterface.QuantStudio5.ViewModels
                 e.Result = processedResults;
             }
         }
+
+
+        public T MultipleTries<T>(Func<T> function)
+        {
+            // https://stackoverflow.com/questions/1563191/cleanest-way-to-write-retry-logic
+
+            var MaxTries = Configuration.FileReadMaxTries;
+            var tries = MaxTries;
+            var delay = Configuration.FileReadDelay;
+            while (true)
+            {
+                try
+                {
+                    return function();
+                }
+                catch(Exception ex)
+                {
+                    if (--tries <= 0) 
+                    {
+                        Logs.Add(new LogModel() { Log = $"Cannot read the file after {MaxTries} tries. Error Message: {ex.Message}" });
+                        throw; 
+                    }
+                    Logs.Add(new LogModel() { Log = $"Failed reading the file. Trying again in {delay} ms. Max tries is set as {MaxTries}" });
+                    System.Threading.Thread.Sleep(delay);
+                }
+            }
+        }
+
         private List<InterfaceResults> ProcessExcelFile(FileStream fileStream)
         {
             List<InterfaceResults> processedInterfaceResults = new List<InterfaceResults>();
@@ -286,6 +312,10 @@ namespace CD4.ExcelInterface.QuantStudio5.ViewModels
                 _backgroundWorker.ReportProgress(1, $"An Error occured {ex.Message}. Excel file not processed!");
                 return new List<InterfaceResults>();
             }
+            finally 
+            {
+                fileStream.Close();
+            }
 
         }
 
@@ -365,11 +395,19 @@ namespace CD4.ExcelInterface.QuantStudio5.ViewModels
 
         private void DeleteIfExists(FileInfo fileInfo)
         {
-            if (fileInfo.Exists)
+            try
             {
-                fileInfo.Delete();
-                Logs.Add(new LogModel() { Log = "Job completed" });
+                if (fileInfo.Exists)
+                {
+                    fileInfo.Delete();
+                    Logs.Add(new LogModel() { Log = "Job completed" });
+                }
             }
+            catch (Exception ex)
+            {
+                Logs.Add(new LogModel() { Log = $"An error while trying to delete the file {fileInfo.Name}: {ex.Message}" });
+            }
+
         }
         private void OnInitiateJob(object sender, ElapsedEventArgs e)
         {
