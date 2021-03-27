4224a592faf24fcbc3c0209f16254f288cdaae9a
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-03-27 13:43:42 +0500

    #103 Added database tables required for implementing contacts

---
+++ CD4.Data/dbo/Tables/ContactDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ContactDetails]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ContactsId] INT NOT NULL,
+	[Contact] varchar(100) not null,
+	[ContactType] int not null, 
+    CONSTRAINT [FK_ContactDetails_Contacts] FOREIGN KEY ([ContactsId]) REFERENCES [dbo].[Contacts]([Id]), 
+    CONSTRAINT [FK_ContactDetails_ContactType] FOREIGN KEY ([ContactType]) REFERENCES [dbo].[ContactType]([Id])
+)
---
+++ CD4.Data/dbo/Tables/Contacts_Sites.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Contacts_Sites]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ContactsId] int not null,
+	[SitesId] int not null, 
+    CONSTRAINT [FK_Contacts-Sites_Contacts] FOREIGN KEY ([ContactsId]) REFERENCES [dbo].[Contacts]([Id]), 
+    CONSTRAINT [FK_Contacts-Sites_Sites] FOREIGN KEY ([SitesId]) REFERENCES [dbo].[Sites]([Id])
+)
