a32391f86a547967626b5b89fa52a0bb1813e926
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-04-11 23:34:55 +0500

    Added a stored porocedure to insert a sample accepted time to sample tracking history to be used via billing

---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertAcceptedDateTimeForSampleFromBilling.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertAcceptedDateTimeForSampleFromBilling]
+	@Cin varchar(50),
+	@AcceptedAt char(19) -- format 'yyyy-MM-dd HH:mm:ss'
+AS
+BEGIN
+	
+	DECLARE @StatusId int;
+	DECLARE @AuditTypeId int;
+
+	-- get the status Id for received to insert the Accepted date for received status
+	SELECT @StatusId = [Id] 
+	FROM [dbo].[Status] 
+	WHERE [Status] = 'Received';
+
+	SELECT @AuditTypeId  = [Id]
+	FROM [dbo].[AuditTypes]
+	WHERE [Description] = 'Sample';
+	
+	INSERT INTO [dbo].[TrackingHistory]([TrackingType],[SampleCin],[StatusId],[UsersId],[TimeStamp])
+	VALUES (@AuditTypeId,@Cin,@StatusId,1,@AcceptedAt);
+
+	-- tracking history
+	INSERT INTO [dbo].[AuditTrail]([AuditTypeId],[Cin],[StatusId],[Details]) 
+	VALUES (@AuditTypeId,@Cin,@StatusId,CONCAT('Sample accepted datetime' ,@AcceptedAt,' inserted as received from billing.'));
+
+END
