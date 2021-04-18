32ef2eae3ff3c3aa79ba92dd9dc554864d260e51
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-04-19 01:35:45 +0500

    database level changes to fix a bug which inserts two sample accepted datetimes which causes apparent row dublication on UI grid

--- CD4.Data/dbo/StoredProcedures/usp_InsertAcceptedDateTimeForSampleFromBilling.sql
+++ CD4.Data/dbo/StoredProcedures/usp_InsertAcceptedDateTimeForSampleFromBilling.sql
@@ -3,12 +3,16 @@
 	@AcceptedAt char(19) -- format 'yyyy-MM-dd HH:mm:ss'
 AS
 BEGIN
+
+/*========= IMPORTANT: DO NOT CALL THIS DIRECTLY. USE [dbo].[DecideAndExecInsertOrUpdateSampleAcceptedDatetime] ===============*/
 	
-	DECLARE @StatusId int;
+	DECLARE @AcceptedStatusId int;
 	DECLARE @AuditTypeId int;
 
+	DECLARE @AcceptedDate DATETIMEOFFSET(7) = CAST(@AcceptedAt AS DATETIME) AT TIME ZONE 'Pakistan Standard Time';
+
 	-- get the status Id for received to insert the Accepted date for received status
-	SELECT @StatusId = [Id] 
+	SELECT @AcceptedStatusId = [Id] 
 	FROM [dbo].[Status] 
 	WHERE [Status] = 'Received';
 
@@ -17,10 +21,10 @@ BEGIN
 	WHERE [Description] = 'Sample';
 	
 	INSERT INTO [dbo].[TrackingHistory]([TrackingType],[SampleCin],[StatusId],[UsersId],[TimeStamp])
-	VALUES (@AuditTypeId,@Cin,@StatusId,1,@AcceptedAt);
+	VALUES (@AuditTypeId,@Cin,@AcceptedStatusId,1,@AcceptedDate);
 
 	-- tracking history
 	INSERT INTO [dbo].[AuditTrail]([AuditTypeId],[Cin],[StatusId],[Details]) 
-	VALUES (@AuditTypeId,@Cin,@StatusId,CONCAT('Sample accepted datetime' ,@AcceptedAt,' inserted as received from billing.'));
+	VALUES (@AuditTypeId,@Cin,@AcceptedStatusId,CONCAT('Sample accepted datetime' ,@AcceptedDate,' inserted as received from billing.'));
 
 END
