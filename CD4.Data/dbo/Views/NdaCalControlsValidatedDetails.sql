5d429548afd4b38a3b8e51c40b375f8c408247ef
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-04-22 15:19:49 +0500

    Flipped dhivehi and english name

--- CD4.Data/dbo/Views/NdaCalControlsValidatedDetails.sql
+++ CD4.Data/dbo/Views/NdaCalControlsValidatedDetails.sql
@@ -2,7 +2,7 @@
 	AS 
 	SELECT 
 		[NAT].[Cin]
-	   ,CONCAT([U].[Fullname], CHAR(13),[U].[FullNameLocal]) AS [QcCalValidatedBy]
+	   ,CONCAT([U].[FullNameLocal], CHAR(13),[U].[Fullname]) AS [QcCalValidatedBy]
 FROM [dbo].[NdaActionTracking] [NAT]
 INNER JOIN [dbo].[Users] [U] ON [NAT].[ActionUserId] = [U].[Id]
 INNER JOIN [dbo].[NdaLookup] [NL] ON [NL].[Id] = [NAT].[NdaLookupId] 
--- CD4.Data/dbo/Views/NdaSampleReceivedDetails.sql
+++ CD4.Data/dbo/Views/NdaSampleReceivedDetails.sql
@@ -2,7 +2,7 @@
 	AS
 	SELECT 
 		 [NAT].[Cin]
-		,CONCAT([U].[Fullname], CHAR(13),[U].[FullNameLocal]) AS [ReceivedBy]
+		,CONCAT([U].[FullNameLocal], CHAR(13),[U].[Fullname]) AS [ReceivedBy]
 	FROM [dbo].[NdaActionTracking] [NAT]
 	INNER JOIN [dbo].[Users] [U] ON [NAT].[ActionUserId] = [U].[Id]
 	INNER JOIN [dbo].[NdaLookup] [NL] ON [NL].[Id] = [NAT].[NdaLookupId] 
