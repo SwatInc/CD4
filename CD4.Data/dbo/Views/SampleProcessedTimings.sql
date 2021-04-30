f33fa316690a355a8e5e26550852fda68a7e216d
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-04-30 16:25:54 +0500

    #136 Added the indexed view [dbo].[SampleProcessedTimings] which will allow implementation of sample processed time on report

---
+++ CD4.Data/dbo/Views/SampleProcessedTimings.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[SampleProcessedTimings]
+	WITH SCHEMABINDING
+	AS SELECT 
+		DISTINCT([r].[Sample_Cin]),
+		MAX([rt].[TimeStamp]) AS [SampleProcessedDateTime]
+FROM [dbo].[Result] [r]
+INNER JOIN [dbo].[TrackingHistory] [rt] ON [rt].[ResultId] = [r].[Id]
+AND [rt].[Statusid] = 4
+GROUP BY [r].[Sample_Cin];
+
+GO
+
+CREATE UNIQUE CLUSTERED INDEX 
+    ucidx_sample_cin
+ON [dbo].[SampleProcessedTimings]([Sample_Cin]);
+
+GO
