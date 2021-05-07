41765637604c54ebe49e5960d40709a9165b4e11
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-05-08 02:07:03 +0500

    data layer changes to add sample and test priority. Sample priority will be completed and only initial set up for test priority to be done

--- CD4.Data/dbo/Tables/Result.sql
+++ CD4.Data/dbo/Tables/Result.sql
@@ -6,6 +6,7 @@
     [Result] NVARCHAR(50) NULL, 
     [ReferenceCode] CHAR(2) NOT NULL DEFAULT 'NM', -- default set as normal(NM) because inital result on table will be null.
     [IsDeltaOk] BIT NOT NULL DEFAULT 1, -- default is NotFailed(1) because initial result is null delta failure cannot be determined unless a result is entered.
+    [IsStat] BIT NOT NULL DEFAULT 0,
     CONSTRAINT [FK_Result_Test] FOREIGN KEY ([TestId]) REFERENCES [dbo].[Test]([Id]), 
     CONSTRAINT [FK_Result_Sample] FOREIGN KEY ([Sample_Cin]) REFERENCES [dbo].[Sample]([Cin]), 
     CONSTRAINT [AK_Result_CinAndTestId] UNIQUE ([Sample_Cin],[TestId]), 
