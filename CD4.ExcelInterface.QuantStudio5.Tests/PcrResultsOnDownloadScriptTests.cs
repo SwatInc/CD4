f349d0a0e3ca38cd8cfdd1719c50776747770105
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-05-13 00:50:03 +0500

    Added tests for QuantStudio 5

---
+++ CD4.ExcelInterface.QuantStudio5.Tests/PcrResultsOnDownloadScriptTests.cs
@@ -0,0 +1,46 @@
+﻿using CD4.ExcelInterface.QuantStudio5.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CD4.ExcelInterface.QuantStudio5.Tests
+{
+    [TestClass]
+    public class PcrResultsOnDownloadScriptTests
+    {
+        [DataTestMethod]
+        [DataRow("41", "41", "41","35", "negative")] //case 1
+        [DataRow("41", "41", "40","35", "negative")] //case 2
+        [DataRow("40", "40", "40", "35", "positive")] //case 3
+        [DataRow("40", "40", "41", "35", "")] //case 4
+        [DataRow("40", "41", "40", "35", "")] //case 5
+        [DataRow("41", "40", "40", "35", "")] //case 6
+        [DataRow("40", "41", "41", "35", "")] //case 7
+        [DataRow("41", "40", "41", "35", "")] //case 8
+        [DataRow("41", "41", "41", "40", "")] //case 9
+        [DataRow("41", "41", "Undetermined", "35", "negative")] //case 1
+        [DataRow("Undetermined", "Undetermined", "40", "35", "negative")] //case 2
+        [DataRow("40", "40", "40", "35", "positive")] //case 3
+        [DataRow("40", "40", "Undetermined", "35", "")] //case 4
+        [DataRow("40", "Undetermined", "40", "35", "")] //case 5
+        [DataRow("Undetermined", "40", "40", "35", "")] //case 6
+        [DataRow("40", "Undetermined", "Undetermined", "35", "")] //case 7
+        [DataRow("Undetermined", "40", "Undetermined", "35", "")] //case 8
+        [DataRow("Undetermined", "Undetermined", "Undetermined", "40", "")] //case 9
+        public void GENEdania_AllTargetsAllCases_PositiveNegativeBlank(string orf1ab, string ngene, string egene,string ic, string actualResult)
+        {
+            var test = new List<MeasurementValues>();
+            test.Add(new MeasurementValues() { TestCode = "ORF1ab", MeasurementValue = orf1ab });
+            test.Add(new MeasurementValues() { TestCode = "N Gene", MeasurementValue = ngene });
+            test.Add(new MeasurementValues() { TestCode = "E Gene", MeasurementValue = egene });
+            test.Add(new MeasurementValues() { TestCode = "IC", MeasurementValue = ic });
+
+            var script = new QuantStudio5OnDownloadScript();
+
+            var interpretedResult = script.GetInterpretation(4, test);
+
+            Assert.IsTrue(interpretedResult.ToLower().Contains(actualResult));
+
+        }
+    }
+}
---
+++ CD4.ExcelInterface.QuantStudio5.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CD4.ExcelInterface.QuantStudio5.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CD4.ExcelInterface.QuantStudio5.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68ab0f5c-20e1-4ca9-b23b-efc151a75a8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
