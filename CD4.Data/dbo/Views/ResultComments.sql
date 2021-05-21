93a85f8fa452ef4095fc06f4bb3a3a4f102d7059
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-05-22 03:13:52 +0500

    created an indexed view which has the result Id and sample comment as string [varchar 1000]

---
+++ CD4.Data/dbo/Views/ResultComments.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[ResultComments]
+WITH SCHEMABINDING
+	AS 
+	SELECT
+	[C].[Identifier] AS [ResultId],
+	[CL].[Description] AS [Comment]
+FROM [dbo].[Comment] [C]
+INNER JOIN [dbo].[CommentList] [CL] ON [C].[CommentListId] = [CL].[Id]
+INNER JOIN [dbo].[CommentType] [CT] ON [C].[CommentTypeId] = [CT].Id
+WHERE [CL].[IsActive] = 1 AND [CT].[Description] = 'Result';
+GO
+
+CREATE UNIQUE CLUSTERED INDEX 
+    [ucidx_RESULT_ID]
+ON [dbo].[ResultComments]([ResultId]);
+GO
