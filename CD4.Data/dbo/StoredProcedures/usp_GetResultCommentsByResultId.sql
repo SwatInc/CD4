cc7ab53ef1dddfcdb2a6e2465690e619f060ba4d
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-05-26 01:30:01 +0500

    Added stored procedures to database for updating and inserting comments to results

---
+++ CD4.Data/dbo/StoredProcedures/usp_GetResultCommentsByResultId.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetResultCommentsByResultId]
+	@ResultId int
+AS
+BEGIN
+	SELECT [ResultId], [Comment] 
+	FROM [dbo].[ResultComments] WITH (NOEXPAND)
+	WHERE 
+		[ResultId] = @ResultId;
+END
---
+++ CD4.Data/dbo/StoredProcedures/usp_InsertResultComment.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertResultComment]
+	@CommentListId int,
+	@ResultId  int,
+	@UserId int
+AS
+BEGIN
+	--insert the comment
+	DECLARE @ResultCommentTypeId int = 3; -- RESULT COMMENT
+	INSERT INTO [dbo].[Comment]([CommentListId],[CommentTypeId],[Identifier],[UserId],[CreatedAt],[UpdatedAt])
+	VALUES (@CommentListId, @ResultCommentTypeId, @ResultId,@UserId,GETDATE(), GETDATE());
+	
+	-- insert audit trail
+
+END
