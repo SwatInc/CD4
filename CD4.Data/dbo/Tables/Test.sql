efbbdd8a21ad28aa672d28831a9188a9ca921257
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-06-11 04:32:47 +0500

    database chamges required to set configurable CD4 lient side sorting of tests by sort order for worksheet, analysis report and test configuration

--- CD4.Data/dbo/Tables/Test.sql
+++ CD4.Data/dbo/Tables/Test.sql
@@ -9,6 +9,7 @@
     [UnitId] INT NOT NULL,
     [Reportable] BIT NOT NULL DEFAULT 1,
     [Code] VARCHAR(3) NOT NULL DEFAULT '', -- this will be suffix like P, F P1
+    [SortOrder] int NOT NULL DEFAULT 0,
     [DefaultCommented] BIT NOT NULL,
     CONSTRAINT [FK_Test_ResultDataType] FOREIGN KEY ([ResultDataTypeId]) REFERENCES [dbo].[ResultDataType]([Id]), 
     CONSTRAINT [AK_Test_Description] UNIQUE ([Description]), 
