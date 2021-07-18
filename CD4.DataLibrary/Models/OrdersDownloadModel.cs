73f11b16b861a2007d01acba175788e199128eff
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-07-18 21:43:29 +0500

    Datalayer code to download all orders for a specified analyser or dowload specific samples' orders for a particular analyser

---
+++ CD4.DataLibrary/Models/OrdersDownloadModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD4.DataLibrary.Models
+{
+    public class OrdersDownloadModel
+    {
+        public string Download { get; set; }
+        public string Sid { get; set; }
+        public bool SamplePriority { get; set; }
+        public bool TestPriority { get; set; }
+        public string EpisodeNumber { get; set; }
+        public string Age { get; set; }
+        public string Fullname { get; set; }
+        public string NidPp { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Address { get; set; }
+
+    }
+}
